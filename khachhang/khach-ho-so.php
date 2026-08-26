cf950dd7ce08ae0286ca7b15ae28390247ebf8de
nguyenhonghanh119 <nguyenhonghanh1192005@gmail.com>
2026-08-26 11:31:54 +0700

    cap nhat tinh nang

--- khachhang/khach-ho-so.php
+++ khachhang/khach-ho-so.php
@@ -8,6 +8,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=Inter:wght@400;500;600;700&family=IBM+Plex+Mono:wght@500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="style.css">
 </head>
+<br/>
 <body>
 <div class="shell">
   <aside class="sidebar">
